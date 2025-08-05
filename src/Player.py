1ab00d453d8c3eefcee3b233e14075b408211e97
AradD7 <arad.delaramnikoo@torontomu.ca>
2025-08-05 16:44:09 -0400

    the very basic game is done. Might add graphics or highlight features later

--- src/Player.py
+++ src/Player.py
@@ -1,5 +1,6 @@
 class Player:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.table = [[0 for _ in range(3)] for _ in range (3)]
         self.links = [] 
         self.total_points = 0
@@ -11,9 +12,10 @@ class Player:
         for i in range(2, -1, -1):
             if not(self.table[i][col]):
                 self.table[i][col] = number
+                self.update_points()
                 list(map(lambda p: p.update(number, col), self.links))
                 return
-        raise Exception("Column is full")
+        raise Exception(f"Column {col} is full")
 
     def link(self, another):
         self.links.append(another)
@@ -21,25 +23,57 @@ class Player:
     
 
     def update(self, number, col):
-        for i in range(2, -1, -1):
-            if self.table[i][col] == number:
-                self.table[i][col] = 0
+        for row in range(2, -1, -1):
+            if self.table[row][col] == number:
+                self.table[row][col] = 0
+        self.update_points()
         self.rearrange(col)
 
     def rearrange(self, col):
-        for i in range(2, 0, -1):
-            if not(self.table[i][col]):
-                check = i - 1
+        for row in range(2, 0, -1):
+            if not(self.table[row][col]):
+                check = row - 1
                 while check >= 0 and not(self.table[check][col]):
                     check -= 1
                 if check >= 0:
-                    self.table[i][col], self.table[check][col] = self.table[check][col], self.table[i][col]
+                    self.table[row][col], self.table[check][col] = self.table[check][col], self.table[row][col]
+
+
+    def update_points(self):
+        self.total_points = 0
+        for col in range(3):
+            multiplier = {}
+            for row in range(3):
+                current_number = self.table[row][col]
+                if current_number in multiplier:
+                    multiplier[current_number] += 1
+                else:
+                    multiplier[current_number] = 1
+            for key in multiplier:
+                self.total_points += (key * multiplier[key]) * multiplier[key]
+
+
+    def isfull(self):
+        for col in range(3):
+            if not(self.table[0][col]):
+                return False
+        return True
 
     def __str__(self):
         str = ""
         for row in range(3):
-            str += f"{self.table[row]}    "
+            str += f"{self.table[row]}\t"
             for link in self.links:
-                str += f"{link.table[row]}    "
+                str += f"{link.table[row]}\t"
             str += '\n'
+        if len(self.name) > 4:
+            str += f"{self.name[:4]}..: {self.total_points}\t"
+        else:
+            str += f"{self.name}: {self.total_points}\t\t"
+        for link in self.links:
+            if len(link.name) > 4:
+                str += f"{link.name[:4]}..: {link.total_points}\t"
+            else:
+                str += f"{link.name}: {link.total_points}\t\t"
+        str += '\n'
         return str
--- src/main.py
+++ src/main.py
@@ -1,17 +1,67 @@
 from Player import Player
+from random import randrange
 
-player1 = Player()
-player2 = Player()
-player1.link(player2)
-player1.place(3,2)
-player2.place(2,2)
-print(player1)
+def init_game():
+    Players = [Player(input("Player 1 Name: ")), Player(input("Player 2 Name: "))]
+    Players[0].link(Players[1])
+    apostrophe = []
+    for player in Players:
+        if player.name[-1].lower() == 's':
+            apostrophe.append("'")
+        else:
+            apostrophe.append("'s")
+    turn = 0
+    return Players, apostrophe, turn
 
-player1.place(3, 2)
-player2.place(6, 2)
 
-print(player1)
+def main():
+    Players, apostrophe, turn = init_game()
+    print("\nThis is Knucklebones!\n")
 
-player1.place(6, 2)
-player2.place(3, 2)
-print(player1)
+    while True:
+
+        print(Players[0])
+        
+        if turn % 2 == 0:
+            turn = 0
+        print(f"It Is {Players[turn].name}{apostrophe[turn]} Turn: (press ENTER to roll) ", end="")
+        
+        input()
+        dice = randrange(1, 7)
+        print(f"You rolled a {dice}!\n")
+
+        col = int(input("Select a Column to Place: "))
+        while True:
+            try:
+                Players[turn].place(dice, col - 1)
+                print()
+                break
+            except Exception as e:
+                print(e)
+                col = int(input("Choose an Available Column: "))
+        
+
+
+        if Players[turn].isfull():
+            max_points = Players[0].total_points
+            winner_idx = 0
+            for i in range(1, len(Players)):
+                if Players[i].total_points > max_points:
+                    max_points = Players[i].total_points
+                    winner_idx = i
+            print(Players[0])
+            print(f"{Players[winner_idx].name} Is The Winner!\n")
+            
+            play_again = input("Want to Play Again? (yes/no) ")
+            print()
+            if play_again == "yes" or play_again == 'y':
+                Players, apostrophe, turn = init_game()
+                continue
+            break
+
+        turn += 1
+
+
+
+
+main()
